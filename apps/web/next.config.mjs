432fb550ef7adf3d5cf7b282a97f6e70b430e8ef
DominikScholz <dominik.scholz@live.at>
2024-10-09 18:17:58 +0200

    chore: remove turbo from nextconfig

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // transpilePackages: ['own.tiles'],
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: [
-            {
-              loader: '@svgr/webpack',
-              options: {
-                dimensions: false
-              }
-            }
-          ],
-          as: '*.js'
-        }
-      }
-    }
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
