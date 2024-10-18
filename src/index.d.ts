d9a0e75f3dc2b27214ff83ce4c3331c4a9c5107e
DominikScholz <dominik.scholz@live.at>
2024-10-18 12:33:39 +0200

    remove compile files

--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from 'InnerOwnTile';
-export * from './types';
-export * from './utils/props';
-export { default as widgets } from './widgets';
--- src/utils/IFrame.d.ts
+++
@@ -1,3 +0,0 @@
-import { FC, IframeHTMLAttributes } from 'react';
-declare const IFrame: FC<IframeHTMLAttributes<HTMLIFrameElement>>;
-export default IFrame;
